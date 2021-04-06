8ee38928942c689de8b5ba0fbb0bac38ea1ffe09
WIN-V7C8M8OPM34\Administrator <archawatip_seeneha@kkumail.com>
2021-04-06 23:18:00 +0700

    ECOM 0.1

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ProductService} from './services/product.service';
+import {HeaderComponent} from './components/header/header.component';
+
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        HeaderComponent,
+        HeaderComponent
+    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
